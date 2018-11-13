b90345d9bd00f031096158b9e24acc988b0815f1
Koki Ando <basket.koki.bowl@gmail.com>
2018-11-13 16:46:26 +1100

    BeautifulSoup scraping scripts added

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,6 +1,8 @@
 import tweepy
 import datetime
 import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
 
 # @doshishauni_inf
 appname = 'DoshishaNews_Bot'
@@ -28,20 +30,52 @@ api = tweepy.API(auth)
 # Tweet content
 
 date = datetime.datetime.now()
-today = "{}年{}月{}日".format(date.year, date.month, date.day)
+today = "{}-{}-{}".format(date.year, date.month, date.day)
+the_day = "2018年11月12日"
 
-doshisha_news = pd.read_csv("/Users/KokiAndo/Desktop/Python/Doshisha/Data/Doshisha_news.csv")
-doshisha_news_today = doshisha_news[doshisha_news['publish_date'] == today]
+# Starts of Scraping Scripts
+
+news_all_url = "https://www.doshisha.ac.jp/news/all/2018/1"
+
+uClient = uReq(news_all_url)
+page_html = uClient.read()
+uClient.close()
+soup = BeautifulSoup(page_html, "html.parser")
+
+date_containers = soup.find("dl", {"class" : "newsList"}).findAll("dt")
+title_containers = soup.find("dl", {"class" : "newsList"}).find_all("a", {"class" : "link01"})
+
+date_list = []
+title_list = []
+url_list = []
+
+for date_container in date_containers:
+    date = date_container.span.text.replace("'", "").replace("年", "-").replace("月", "-").replace("日", "")
+    date_list.append(date)
+
+for title_container in title_containers:
+    title = title_container.text
+    title_list.append(title)
+
+for title_container in title_containers:
+    title_url = title_container["href"]
+    url_list.append(title_url)
+    # url_list.append(title_url)
+
+# End of scraping Scripts
+
+
+doshisha_news = pd.DataFrame({"publish_date" : date_list, "title" : title_list, "url" : url_list})
+doshisha_news_today = doshisha_news[doshisha_news['publish_date'] == today[2:]]
 
 if len(doshisha_news_today) is 0:
     pass
 else:
     for i in range(0, len(doshisha_news_today)):
-        publish_date = doshisha_news_today.iloc[[i]]['publish_date']
-        news_type = doshisha_news_today.iloc[[i]]['type']
-        title = doshisha_news_today.iloc[[i]]['content_title']
-        title_url = doshisha_news_today.iloc[[i]]['content_url']
+        Publish_Date = doshisha_news_today.iloc[[i]]['publish_date']
+        Content_Title = doshisha_news_today.iloc[[i]]['title']
+        Title_Url = doshisha_news_today.iloc[[i]]['url']
 
-        msg = "{}:{}\n{}\n{}".format(publish_date.values, news_type.values, title.values, title_url.values)
+        msg = "{}\n{}\n https://www.doshisha.ac.jp{}".format(Publish_Date.values, Content_Title.values, Title_Url.values)
         # print(msg.replace("['", "").replace("']", ""))
         api.update_status(msg.replace("['", "").replace("']", ""))
