fbc0b8c809926783148c6433a6bb93cc29c41dc5
Koki Ando <basket.koki.bowl@gmail.com>
2018-12-05 21:23:43 +1100

    some experimental scripts

--- test.py
+++ test.py
@@ -1,22 +1,44 @@
+# coding: utf-8
+import os
+import tweepy
 import datetime
 import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
 
-date = datetime.datetime.now()
-today = "{}-{}-{}".format(date.year, date.month, date.day)
-day = '2018-11-08'
-the_other_day = '2018-08-22'
+commerce_url = "https://com.doshisha.ac.jp/news/news/"
+economics_url = "https://www.econ.doshisha.ac.jp/news/topics/"
+letter_url = "https://letters.doshisha.ac.jp/"
+gc_url = "https://globalcommunications.doshisha.ac.jp/news"
+gr_url = "https://gr.doshisha.ac.jp/"
+biomedic_url = "https://biomedical.doshisha.ac.jp/"
 
-doshisha_news = pd.read_csv("/Users/KokiAndo/Desktop/Python/Doshisha/Data/Doshisha_news.csv")
-doshisha_news_today = doshisha_news[doshisha_news['publish_date'] == the_other_day]
+faculty_link_list = [commerce_url, economics_url, letter_url, gc_url, gr_url, biomedic_url]
 
-if len(doshisha_news_today) is 0:
-    pass
-else:
-    for i in range(0, len(doshisha_news_today)):
-        publish_date = doshisha_news_today.iloc[[i]]['publish_date']
-        news_type = doshisha_news_today.iloc[[i]]['type']
-        title = doshisha_news_today.iloc[[i]]['content_title']
-        title_url = doshisha_news_today.iloc[[i]]['content_url']
 
-        msg = "{}:{}\n{}\n{}".format(publish_date.values, news_type.values, title.values, title_url.values)
-        print(msg.replace("['", "").replace("']", ""))
+for i in range(len(faculty_link_list)):
+
+    url = faculty_link_list[i]
+    uClient = uReq(url)
+    page_html = uClient.read()
+    uClient.close()
+    soup = BeautifulSoup(page_html, "html.parser")
+
+    if url == faculty_link_list[0] or url == faculty_link_list[1]:
+        date_containers = soup.find("dl", {"class" : "newsList2 clearfix"}).findAll("dt")
+        title_containers = soup.find("dl", {"class" : "newsList2 clearfix"}).find_all("a", {"class" : "link01"})
+    elif url == faculty_link_list[2] or url == faculty_link_list[5]:
+        date_containers = soup.find("dl", {"class" : "tab1 newsList"}).findAll("dt")
+        title_containers = soup.find("dl", {"class" : "tab1 newsList"}).find_all("a", {"class" : "link01"})
+    elif url == faculty_link_list[3]:
+        date_containers = soup.findAll("p", {"class" : "date"})
+        title_containers = soup.findAll("p", {"class" : "txt"})
+    elif url == faculty_link_list[4]:
+        date_containers = soup.find("dl", {"class" : "newsList2"}).findAll("dt")
+        title_containers = soup.find("dl", {"class" : "newsList2"}).find_all("a", {"class" : "link01"})
+    else:
+        pass
+
+    date_list = []
+    title_list = []
+    url_list = []
